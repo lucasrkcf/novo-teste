330f9ce648ad3e724c0eb07fc712f17c1b751087
Lucas Rafaele da Silva <lucas.rafaele@kroton.com.br>
2019-06-28 15:17:55 -0300

    v10

--- ProAgil.API/Controllers/ValuesController.cs
+++ ProAgil.API/Controllers/ValuesController.cs
@@ -7,65 +7,48 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProAgil.Repository;
 
-namespace ProAgil.API.Controllers
-{
-    [Route("api/[controller]")]
+namespace ProAgil.API.Controllers {
+    [Route ("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
-    {
-       public readonly ProAgilContext _context;
-        public ValuesController(ProAgilContext context)
-        {
+    public class ValuesController : ControllerBase {
+        public readonly ProAgilContext _context;
+        public ValuesController (ProAgilContext context) {
             _context = context;
 
         }
         // GET api/values
         [HttpGet]
-        public async Task <IActionResult> Get()
-        {
-            try
-            {
-                var results =  await _context.Eventos.ToListAsync();
-                 return Ok(results);
+        public async Task<IActionResult> Get () {
+            try {
+                var results = await _context.Eventos.ToListAsync ();
+                return Ok (results);
+            } catch (System.Exception) {
+                return this.StatusCode (StatusCodes.Status500InternalServerError, "Banco de dados falhou");
             }
-            catch (System.Exception)
-            {
-              return this.StatusCode(StatusCodes.Status500InternalServerError,"Banco de dados falhou");  
-            }
-            
+
         }
 
         // GET api/values/5
-        [HttpGet("{id}")]
-        public async Task <IActionResult> Get (int id)
-        {
-            try
-            {
-                var results =  await _context.Eventos.FirstOrDefaultAsync(x => x.Id == id);
-                 return Ok(results);
-            }
-            catch (System.Exception)
-            {
-              return this.StatusCode(StatusCodes.Status500InternalServerError,"Banco de dados falhou");  
+        [HttpGet ("{id}")]
+        public async Task<IActionResult> Get (int id) {
+            try {
+                var results = await _context.Eventos.FirstOrDefaultAsync (x => x.Id == id);
+                return Ok (results);
+            } catch (System.Exception) {
+                return this.StatusCode (StatusCodes.Status500InternalServerError, "Banco de dados falhou");
             }
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+        public void Post ([FromBody] string value) { }
 
         // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        [HttpPut ("{id}")]
+        public void Put (int id, [FromBody] string value) { }
 
         // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        [HttpDelete ("{id}")]
+        public void Delete (int id) { }
     }
 }
--- ProAgil.Repository/IProAgilRepository.cs
+++ ProAgil.Repository/IProAgilRepository.cs
@@ -9,16 +9,15 @@ namespace ProAgil.Repository
 
          void Update<T>(T entity) where T : class;
 
-         void Delete<T>(T entity) where
-        Task GetAllEventoAsync(bool v);T : class;
-         
+         void Delete<T>(T entity) where T : class;
+             
          Task<bool> SaveChangesAsync();
 
          // Eventos
          Task<Evento[]> GetAllEventoAsyncByTema(string tema, bool includePalestrantes);
          Task<Evento[]> GetAllEventoAsync(bool includePalestrantes);
          Task<Evento> GetEventoAsyncById(int EventoId, bool includePalestrantes);
-        Task GetAllEventoAsyncById(int eventoId, bool v);
+      
 
         //Palestrante
         Task<Palestrante[]> GetAllPalestrantesAsyncByName(bool includeEventos);
