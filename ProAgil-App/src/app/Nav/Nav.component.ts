e9a2bdaf4aa433d16ddaa0cef818c8d6deca7bf5
Lucas Rafaele da Silva <lucas.rafaele@kroton.com.br>
2019-07-02 17:24:24 -0300

    v12

--- ProAgil-App/src/app/Nav/Nav.component.ts
+++ ProAgil-App/src/app/Nav/Nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+
   selector: 'app-Nav',
   templateUrl: './Nav.component.html',
   styleUrls: ['./Nav.component.css']
--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { EventoService } from '../_services/evento.service';
+import { Evento } from '../_models/Evento';
 
 @Component({
   selector: 'app-eventos',
@@ -8,6 +9,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class EventosComponent implements OnInit {
 
+// tslint:disable-next-line: variable-name
   _filtroLista: string;
   get filtroLista(): string {
     return this._filtroLista;
@@ -17,33 +19,35 @@ export class EventosComponent implements OnInit {
     this._filtroLista = value;
     this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
   }
-  eventosFiltrados: any = [];
-  eventos: any = [];
+  eventosFiltrados: Evento[];
+  eventos: Evento[];
   imagemLargura = 50;
   imagemMargem = 2;
   mostrarImagem = false;
 
 
-  constructor(private http: HttpClient) { }
+  constructor(private eventoService: EventoService ) { }
 
   ngOnInit() {
     this.getEventos();
   }
 
-  filtrarEventos(filtratPor: string): any{
+  filtrarEventos(filtratPor: string): Evento[] {
     filtratPor = filtratPor.toLocaleLowerCase();
     return this.eventos.filter(
       evento => evento.tema.toLocaleLowerCase().indexOf(filtratPor) !== -1
-      
+
     );
   }
-  alternarImgem(){
+  alternarImgem() {
     this.mostrarImagem = !this.mostrarImagem;
   }
 getEventos() {
-  this.http.get('http://localhost:5000/api/values').subscribe(response => {
-    this.eventos = response;
-    console.log(response);
+  this.eventoService.getAllEvento().subscribe(
+    (_eventos: Evento[]) => {
+    this.eventos = _eventos;
+    this.eventosFiltrados = this.eventos;
+    console.log(_eventos);
 }, error => {
   console.log(error);
 });
