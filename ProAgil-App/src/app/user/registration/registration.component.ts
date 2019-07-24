104afb8f0163df0779c6fa3d49e8bccd59fb958e
Lucas Rafaele da Silva <lucas.rafaele@kroton.com.br>
2019-07-24 17:50:38 -0300

    v29

--- ProAgil-App/src/app/user/registration/registration.component.ts
+++ ProAgil-App/src/app/user/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-
-  constructor() { }
+  registerForm: FormGroup;
+  constructor(public fb: FormBuilder, private toastr: ToastrService) { }
 
   ngOnInit() {
+    this.validation();
+  }
+
+  validation() {
+    this.registerForm = this.fb.group({
+fullName : ['', Validators.required],
+email : ['', Validators.required, Validators.email],
+userName : ['', Validators.required],
+passwords : this.fb.group({
+  password : ['', Validators.required, Validators.minLength(4)],
+  confirmPasswor : ['', Validators.required]
+})
+
+    });
+  }
+
+  cadastrarUsuario() {
+    console.log('Cadastrar Usuario');
   }
 
 }
